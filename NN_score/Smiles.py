084bcd0232f243ca110ee0ac9330b284cae01720
Diego Balam Sanchez Espinosa <152104666+DiegoBalamS@users.noreply.github.com>
2024-01-24 02:41:55 -0500

    Update Smiles.py

--- NN_score/Smiles.py
+++ NN_score/Smiles.py
@@ -1,8 +1,28 @@
-class SMILESModel(torch.nn.Module):
-    def __init__(self):
-        super(SMILESModel, self).__init__()
-        # Inicializar el modelo SMILES-Transformer aquí
+import numpy as np
+import pandas as pd
 
-    def forward(self, smiles_string):
-        # Procesar la cadena SMILES y devolver el vector
-        return vector_from_smiles(smiles_string)
+def vocab(smile_file):
+    df=pd.read_table(smile_file)
+    smiles=df['smiles'].values
+    pro_sms=[]
+    for sm in smiles:
+        sm = ' '.join(list(sm))
+        before = before = ['C l -', 'C l', 'O -', 'N +', 'n +', 'B r -', 'B r', 'N a +', 'N a', 'I -', 'S i']
+        after = ['Cl-', 'Cl', 'O-', 'N+', 'n+', 'Br-', 'Br', 'Na+', 'Na', 'I-', 'Si']
+        for b,a in zip(before, after):
+            sm = sm.replace(b, a)
+        pro_sms.append(sm)
+
+    df['processed_smiles']=pro_sms
+    
+    vocab=[]
+    for sm in smiles:
+        l=sm.split(' ')
+        for w in l:
+            if w not in vocab:
+                vocab.append(w)
+
+    with open('project/vocab.txt','a') as f:
+        for v in vocab:
+            f.write(v+'\n')
+
