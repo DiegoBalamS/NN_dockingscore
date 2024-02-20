3384e0ae2e0e15f41026d484832bf8045d5b2072
Diego Balam Sanchez Espinosa <152104666+DiegoBalamS@users.noreply.github.com>
2024-02-20 01:17:02 -0500

    Add files via upload

---
+++ NN_score/__init__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import PackageNotFoundError, version
+
+from .pretrain_trfm import TrfmSeq2seq
+
+from .dockschnet import dockschnet 
+
+From .lightningmodule import MyModel, MyDataset
